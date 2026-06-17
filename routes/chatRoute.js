9551b30cf81736ff3841a76e93a26d60aa98aa44
Sreehari K P <Sreehari.KP@gnapi.tech>
2026-06-17 16:13:03 +0530

    Gemini answers in plain text

--- routes/chatRoute.js
+++ routes/chatRoute.js
@@ -1,5 +1,7 @@
 import express from "express"
 import { retrieveContext } from "../services/retrieveContext.js"
+import { model } from "../config/llm.js";
+
 
 const router = express.Router();
 
@@ -8,8 +10,12 @@ router.post("/ask", async(req,res) => {
         const {question} = req.body;
 
         const context =  await retrieveContext(question);
+        const prompt = `You are a helpful assistant.Answer the user's question using only the context below.Context: ${context.join("\n\n")} Question:${question}`;
+        const response = await model.invoke(prompt);
 
-        res.json({question, context});
+
+        res.set("Content-Type","text/plain")
+        res.send(`Question: ${question}\nAnswer: ${response.content}`);
     } catch(error) {
         res.status(500).json({error: error.message});
     }
