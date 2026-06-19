cb49e97e37f5459ad06fe702366edfe9ad5037d0
Sreehari K P <Sreehari.KP@gnapi.tech>
2026-06-19 11:31:53 +0530

    Stored the port in an env

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express"
 import uploadRoutes from "./routes/upload.js"
 import askRoutes from "./routes/chatRoute.js"
+import dotenv from "dotenv"
+dotenv.config()
 
 const app = express();
 
@@ -9,7 +11,7 @@ app.use("/", uploadRoutes);
 app.use("/", askRoutes);
 
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on PORT ${process.env.PORT}`);
 });
 
--- services/indexDocument.js
+++ services/indexDocument.js
@@ -23,6 +23,5 @@ export async function indexDocument(docs) {
     });
 
     return splitsDocs.length;
-
 }
 
